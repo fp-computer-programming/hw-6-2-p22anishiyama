78bfd266fdebf0599d728ec8647471eaadbcb43b
p22anishiyama <p22anishiyama@fairfieldprep.org>
2021-11-09 10:13:52 -0500

    add hw6-2.py

---
+++ hw6-2.py
@@ -0,0 +1,32 @@
+# Author: ATN 11/9/2021
+
+# Question 1
+lst1 = [0, 1, 2, 3, 4, 5, 6, 7]
+
+# Question 2
+print(len(lst1))
+
+# Question 3
+print(lst1[2])
+
+# Question 4
+print(5 not in lst1)
+
+# Question 5
+print(10 not in lst1)
+
+# Question 6
+lst2 = ["Aiden", "Mr. Mesquita", "Ryan", "Jordan"]
+
+# Question 7
+print(lst2[0])
+
+# Question 8
+print(lst2 * 3)
+
+# Question 9
+
+print(lst1 + lst2)
+
+# Question 10
+print(len(lst2))
